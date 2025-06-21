1223ca043eb6f5d222c5ab71c60c65a4f371a960
Zayakind <63289485+Zayakind@users.noreply.github.com>
2025-06-21 16:29:42 +0300

    #4 Fix antivirus alert (#4)
    Co-authored-by: zayakin <zayakin@sbermegamarket.ru>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(all(not(debug_assertions), not(feature = "console")), windows_subsystem = "windows")]
 
 mod notes;
 mod error;
